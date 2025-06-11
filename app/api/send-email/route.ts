0bd8c78bac16f4fec744e8e9edd5e13abfe7f623
Vahan <vahanm21@gmail.com>
2025-06-11 06:17:01 -0700

    last fix

--- app/api/send-email/route.ts
+++ app/api/send-email/route.ts
@@ -7,6 +7,8 @@ const resend = new Resend("re_FbuwKxvW_J8yCLdCkgRoY7Ek3ooca6yG4");
 export async function POST(request: Request) {
     const { to, subject, text } = await request.json();
 
+    console.log(to, "to")
+
     const data = await resend.emails.send({
         from: 'contact@tdevspace.am',
         to: "tatevavalyan050@gmail.com",
