b067065a746f9ba489ea28972b49d26a7addc171
Vahan <vahanm21@gmail.com>
2025-06-11 06:17:59 -0700

    last fix1

--- app/api/send-email/route.ts
+++ app/api/send-email/route.ts
@@ -5,9 +5,7 @@ import { NextResponse } from 'next/server';
 const resend = new Resend("re_FbuwKxvW_J8yCLdCkgRoY7Ek3ooca6yG4");
 
 export async function POST(request: Request) {
-    const { to, subject, text } = await request.json();
-
-    console.log(to, "to")
+    const { subject, text } = await request.json();
 
     const data = await resend.emails.send({
         from: 'contact@tdevspace.am',
